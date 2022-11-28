17033792e9e4893e5dcf0688420d9e1640567b01
diego tadeu <diegotadsil@gmail.com>
2022-11-28 00:02:08 -0300

    redução de páginas e adição de links externos

--- app.py
+++ app.py
@@ -2,56 +2,12 @@ import json
 import os
 
 import pandas as pd
-import plotly
-import plotly.express as px
-import plotly.graph_objects as go
-import plotly.io as pio
 import pickle
 from flask import Flask, jsonify, redirect, render_template, request, url_for
 from pycaret.classification import *
 
 app = Flask(__name__)
 
-df = pd.read_csv('static/planilha.csv')
-
-fig1 = pio.read_json("static/grafico1.json")
-graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
-
-fig2 = go.Figure(data=[go.Sankey(
-    node = dict(
-      pad = 15,
-      thickness = 100,
-      line = dict(color = "black", width = 0.5),
-      label = ["Palmeiras","Corinthians","Internacional","Atlético MG","Fluminense","Atlético PR","São Paulo","Santos",
-                "Flamengo","Botafogo","Red Bull Bragantino","Goiás","Cuiabá","Coritiba","América MG","Avaí","Ceará","Atlétigo GO",
-                "Juventude","Fortaleza","Betfair","Galera.bet","Estrelabet","Betano","Sportsbet.io","Pixbet","Blaze.bet","Betpix365",
-                "LuckSports.bet","Dafabet","Betcris","Amulerobet"],
-      color = "#310c6b"
-    ),
-    link = dict(
-      source = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31], # indices correspond to labels, eg A1, A2, A1, B1, ...
-      target = [20,21,22,23,23,22,24,25,25,26,27,25,28,29,25,25,30,31,25,30],
-      value =  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-  ))])
-
-#fig2.update_layout(title_text="Basic Sankey Diagram", font_size=25)
-graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
-
-fig3 = px.scatter_geo(df,lon='longitude_decimal',lat="latitude_decimal",hover_name="Times", scope="south america",color="Times",)
-#pio.write_json(fig3,file="static/grafico2.json")
-graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
-
-fig4 = px.scatter_geo(df,lon='longitude_decimal',lat="latitude_decimal",hover_name="Times", scope="south america",
-        color="Times", size=[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],opacity=0.5,symbol="Estádio",center={"lat":-15,"lon":-50})
-graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
-
-fig5 = px.scatter_geo(df,lon='longitude_decimal',lat="latitude_decimal",hover_name="Times", scope="south america",
-        color="Times", size=[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],opacity=0.5,symbol="Estádio",center={"lat":-15,"lon":-50})
-graph5JSON = json.dumps(fig5, cls=plotly.utils.PlotlyJSONEncoder)
-
-fig6 = px.scatter_geo(df,lon='longitude_decimal',lat="latitude_decimal",hover_name="Times", scope="south america",
-        color="Times", size=[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],opacity=0.5,symbol="Estádio",center={"lat":-15,"lon":-50})
-graph6JSON = json.dumps(fig6, cls=plotly.utils.PlotlyJSONEncoder)
 
 modelo = load_model('static/assets/models/my_best_pipeline')
 colunas = ['ITEM_ID','ALTURA','CAPACIDADE_(L)','COMPOSICAO','COR','FORMATO','LARGURA','MARCA',
@@ -76,12 +32,6 @@ def teste2():
         prediction = 'Não interessante'
     return render_template("teste.html",pred=f'Esse perfil foi classificado como {prediction}')
 
-
-
-
-@app.route("/bets")
-def index():
-    return render_template("bet.html",graph1JSON=graph1JSON,graph2JSON=graph2JSON,graph3JSON=graph3JSON,graph4JSON=graph4JSON,graph5JSON=graph5JSON,graph6JSON=graph6JSON)
 
 @app.route("/")
 def resume():
