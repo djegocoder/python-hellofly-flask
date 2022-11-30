32732430c1679a74e6f9559d877808a82bb6a18c
diego tadeu <diegotadsil@gmail.com>
2022-11-30 17:46:15 -0300

    separação de apps

--- app.py
+++ app.py
@@ -1,37 +1,11 @@
-import json
 import os
-
+import numpy as np
 import pandas as pd
 import pickle
 from flask import Flask, jsonify, redirect, render_template, request, url_for
-from pycaret.classification import *
+from pycaret.classification import load_model, predict_model
 
 app = Flask(__name__)
-
-
-modelo = load_model('static/assets/models/my_best_pipeline')
-colunas = ['ITEM_ID','ALTURA','CAPACIDADE_(L)','COMPOSICAO','COR','FORMATO','LARGURA','MARCA',
-'PARA_LAVA_LOUCAS','PARA_MICRO_ONDAS','PESO','PROFUNDIDADE','TEMPO_GARANTIA','TEM_FERRO_FUNDIDO','TEM_GRELHA',
-    'TEM_TAMPA','TIPO_PRODUTO','TIPO_WOK','SESSION_ID','ITEM_PRICE']
-
-@app.route("/teste")
-def teste():
-    return render_template("teste.html")
-
-
-@app.route("/teste2",methods=['POST'])
-def teste2():
-    int_features = [x for x in request.form.values()]
-    final = np.array(int_features)
-    data_unseen = pd.DataFrame([final], columns = colunas)
-    prediction = predict_model(modelo, data=data_unseen, round = 0)
-    prediction = prediction.Label[0]
-    if prediction == 1.0:
-        prediction = 'Interessante'
-    else:
-        prediction = 'Não interessante'
-    return render_template("teste.html",pred=f'Esse perfil foi classificado como {prediction}')
-
 
 @app.route("/")
 def resume():
@@ -74,5 +48,5 @@ def portifolio9():
     return render_template("portifolio-9.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT",5001))
     app.run(debug=True, host='0.0.0.0', port=port)
